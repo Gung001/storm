c750b368b628610a70af06398e82777f88a6673f
Gryant <gung001@163.com>
2018-12-23 16:40:10 +0800

    springboot integrate hbase

--- src/main/java/com/lxgy/storm/controller/MapController.java
+++ src/main/java/com/lxgy/storm/controller/MapController.java
@@ -3,9 +3,11 @@ package com.lxgy.storm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
+ *
+ * springboot integrate hbase
+ * https://github.com/SpringForAll/spring-boot-starter-hbase#%E4%BD%BF%E7%94%A8%E6%96%B9%E5%BC%8F
  * @author Gryant
  */
 @Controller
